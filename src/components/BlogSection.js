82c938ce2015c8defb57221fc24ad987ce47f2c6
vipransh <vipranshverma385@gmail.com>
2024-04-15 09:50:06 +0530

    hashnode graphql api updated

--- src/components/BlogSection.js
+++ src/components/BlogSection.js
@@ -1,16 +1,22 @@
 import React,{useEffect,useState} from 'react'
 
+
 function BlogSection() {
     const [blogs,setBlogs]=useState();
     const query = `
     {
-      user(username: "Vipransh") {
-        publication {
-          posts{
-            slug
-            title
-            brief
-            coverImage
+      publication(host: "vipranshverma.hashnode.dev") {
+        posts(first: 10) {
+          edges {
+            node {
+              title
+              brief
+              url
+              coverImage {
+                url
+                attribution
+              }
+            }
           }
         }
       }
@@ -21,7 +27,8 @@ function BlogSection() {
 
   useEffect(()=>{
     const fetchPosts = async () => {
-        const response = await fetch('https://api.hashnode.com', {
+       try {
+        const response = await fetch(`https://gql.hashnode.com`, {
           method: 'POST',
           headers: {
             'Content-type': 'application/json',
@@ -29,8 +36,11 @@ function BlogSection() {
           body: JSON.stringify({ query }),
         })
         const ApiResponse = await response.json();
-        setBlogs(ApiResponse.data.user.publication.posts);
-        // console.log(ApiResponse.data.user.publication.posts); 
+        setBlogs(ApiResponse.data?.publication?.posts?.edges);
+        // console.log(ApiResponse); 
+       } catch (error) {
+        console.log(error);
+       }
       };
     fetchPosts()
   },[query])
@@ -48,11 +58,11 @@ function BlogSection() {
                 {
                     blogs && blogs.map((data,index)=>(
                         <div key={index} className='flex flex-col md:flex-row  gap-5 p-2 w-[90%] md:w-4/5 border border-black-500 shadow-lg rounded-lg dark:bg-white'>
-                            <img className='w-96 border border-black-400 rounded-lg' src={data.coverImage} alt='blog-img'/>
+                            <img className='w-96 border border-black-400 rounded-lg' src={data.node?.coverImage?.url} alt='blog-img'/>
                             <div>
-                                <h1 className='font-bold '>{data.title}</h1>
-                                <p className='mt-4 mb-4 text-gray-600'>{data.brief}</p>
-                                <a className=' bg-teal-600 text-white px-2 py-1 rounded-xl' target='blank' href={`https://vipranshverma.hashnode.dev/${data.slug}`}>View Post</a>
+                                <h1 className='font-bold '>{data.node?.title}</h1>
+                                <p className='mt-4 mb-4 text-gray-600'>{data.node?.brief}</p>
+                                <a className=' bg-teal-600 text-white px-2 py-1 rounded-xl' target='blank' href={data.node?.url}>View Post</a>
                             </div>
                         </div>
                     ))
